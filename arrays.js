907cd4c34b51a591d24d3d1752057c6959123f84
Zachary M Waxman <zwaxman@gmail.com>
2019-07-16 22:54:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
 
 function addElementToBeginningOfArray (arr,el){
-  
+  return [el,...arr]
 }
 
 function destructivelyAddElementToBeginningOfArray(arr,el){
