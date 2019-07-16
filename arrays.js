5183a7cb53c47fae1b9f17b3b858328fff9b0e6c
Zachary M Waxman <zwaxman@gmail.com>
2019-07-16 22:53:01 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
+
+function addElementToBeginningOfArray (arr,el){
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(arr,el){
+  
+}
